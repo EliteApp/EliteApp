46c852e6591b6f55d1b2aafaf5747e6adef9bc7e
nathaliemessner <nathaliemessner2001@gmail.com>
2018-06-14 11:16:19 +0200

    changed layout

--- app/src/main/java/com/einhard_gymnasium/eliteapp/LogInActivity.java
+++ app/src/main/java/com/einhard_gymnasium/eliteapp/LogInActivity.java
@@ -2,6 +2,7 @@ package com.einhard_gymnasium.eliteapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        ImageView logo = findViewById(R.id.loginLogo);
+        logo.setImageResource(R.drawable.logo_einhard);
     }
 }
