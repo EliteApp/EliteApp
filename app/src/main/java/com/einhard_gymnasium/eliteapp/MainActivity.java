1b040f84194ede22e8c648a5a24a0bbcc462e870
NickLJackson <nick.ligmann1503@gmail.com>
2018-06-20 23:48:40 +0200

    got navigation drawer working

--- app/src/main/java/com/einhard_gymnasium/eliteapp/MainActivity.java
+++ app/src/main/java/com/einhard_gymnasium/eliteapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.einhard_gymnasium.eliteapp;
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentTransaction;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,10 +14,18 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle mToggle;
 
+    private android.support.v7.widget.Toolbar toolbar;
+
+    private FragmentTransaction fragmentTransaction;
+
+    private NavigationView navView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.navigation_action);
+        setSupportActionBar(toolbar);
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
@@ -23,6 +34,28 @@ public class MainActivity extends AppCompatActivity {
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_container, new VertretungsFragment());
+        fragmentTransaction.commit();
+
+        navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_vertretung:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new VertretungsFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Vertretungsplan");
+                        item.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     @Override
